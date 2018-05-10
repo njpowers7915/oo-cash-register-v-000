4d6b9e2c06c11cf62fe851884736db8bdaec7995
Nicholas Powers <njpowers7915@gmail.com>
2018-05-10 23:50:31 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,41 @@
+class CashRegister
+  attr_accessor :total
+  attr_reader :discount
+  @item_hash = {}
+  
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(title, price, quantity = nil)
+    transaction = [title, price, quantity]
+    
+    if quantity != nil
+      @item_hash.nil? ? @item_hash = [title] * quantity : @item_list += [title] * quantity
+      @total += @price * quantity.to_f
+    else
+      @item_list.nil? ? @item_list = [title] : @item_list << [title]
+      @total += @price
+    end
+  end
+  
+  def apply_discount
+    if @discount != nil
+      @total -= (@discount * 10)
+      "After the discount, the total comes to $#{@total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+  
+  def items
+    @item_list
+  end
+  
+  def void_last_transaction
+    
 
+end
+  
+  
