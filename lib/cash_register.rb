b531d42a1969269a7e969e99d3e1e1e915d65c35
Nicholas Powers <njpowers7915@gmail.com>
2018-05-11 00:06:17 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
   attr_accessor :total
   attr_reader :discount
   @item_array = []
-  @price_array = [1, 2]
+  @price_array = []
   
   def initialize(discount = nil)
     @total = 0
@@ -14,11 +14,11 @@ class CashRegister
       @item_array.nil? ? @item_array = [title] * quantity : @item_array += [title] * quantity
       transaction_price = price * quantity.to_f
       @total += transaction_price
-      @price_array += [transaction_price]
+      @price_array.nil? ? @price_array = transaction_price : @price_array += transaction_price
     else
       @item_array.nil? ? @item_array = [title] : @item_array << [title]
       @total += price
-      @price_array += [price]
+      @price_array.nil? ? @price_array = price : @price_array += price
     end
   end
   
