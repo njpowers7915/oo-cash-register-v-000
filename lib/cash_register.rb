4d94729e82aaa3d42a8d7fd4434b59e31251274b
Nicholas Powers <njpowers7915@gmail.com>
2018-05-11 00:03:41 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
   attr_accessor :total
   attr_reader :discount
   @item_array = []
-  @price_array = ["0"]
+  @price_array = [1, 2]
   
   def initialize(discount = nil)
     @total = 0
